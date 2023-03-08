d181947c20a3e728bdf9f3545c110e0007c0a99a
MichelaRicci <michela.ricci.1991@gmail.com>
2023-03-08 22:37:41 +0100

    show + create fixed

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,45 +4,54 @@
 <div class="container">
     <form action="{{ route('comics.store') }}" method="post">
         @csrf
-        <div class="row cols-3">
-            <div class= "col d-flex m-3">
-                <label for="title">Comic Title:</label><input type="text" name="title" id="title" placeholder="Title" required/>
-            </div>
-            
-            <div class= "col d-flex m-3">
-               <label for="price">Price:</label><input type="number"  id="price" name="price" placeholder="Price" required />
+        <div class="row row-cols-3">
+            <div class= "m-3">
+                <label class="form-label" for="title">Comic Title: </label>
+                <input class="ms-2" type="text" name="title" id="title" placeholder="Title" required/>
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="series">Serie</label><input type="text" id="series" name="series"  placeholder="Text" required />
+               <label for="price">Price: </label>
+               <input class="ms-2" type="number"  id="price" name="price" placeholder="Price" required />
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="type">Type:</label><input type="text" name="type" id="type" placeholder="Enter type" required/>
+                <label for="series">Serie: </label>
+                <input class="ms-2" type="text" id="series" name="series"  placeholder="Text" required />
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="sale_date">Release:</label><input type="text" id="sale_date" name="sale_date" placeholder="Release date" required>
+                <label for="type">Type: </label>
+                <input class="ms-2" type="text" name="type" id="type" placeholder="Enter type" required/>
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="text">Artists:</label><input type="text" name="artists" placeholder="Artists' names" required>
+                <label for="sale_date">Release: </label>
+                <input class="ms-2" type="text" id="sale_date" name="sale_date" placeholder="AAAA-MM-DD" required>
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="writers">Writers</label><input type="text" name="writers"  id="writers" placeholder="Writers' names" required/>
+                <label for="text">Artists: </label>
+                <input class="ms-2" type="text" name="artists" placeholder="Artists' names" required>
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="description">Description:</label><textarea type="text" name="description" id="description" placeholder="Description"></textarea> 
+                <label for="writers">Writers: </label>
+                <input class="ms-2" type="text" name="writers"  id="writers" placeholder="Writers' names" required/>
             </div>
 
             <div class= "col d-flex m-3">
-                <label for="Thumbs">Image:</label><textarea type="url"  name="thumb" id="thumb" placeholder="Image url"></textarea> 
+                <label for="description">Description: </label>
+                <textarea class="ms-2" type="text" name="description" id="description" placeholder="Description"></textarea> 
+            </div>
+
+            <div class= "col m-3">
+                <label for="Thumbs">Image: </label>
+                <textarea class="ms-2" type="url"  name="thumb" id="thumb" placeholder="Image url"></textarea> 
             </div>
             
             <div class= "col-12 d-flex m-3">
-                <button type= "submit" href=""class="bg-primary text-uppercase text-white fw-bold load-btn">
+                <button type= "submit" href="" class="bg-primary text-uppercase text-white fw-bold load-btn">
                     <h4>Add comic</h4>
                 </button>
             </div>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,15 +2,23 @@
 @section('title', $comic->title)
 
 @section('main-content')
-<ul>
-    <li>Title:{{ $comic->title }}</li>
-    <li><p>Description:{{ $comic->description }}</p></li>
-    <li><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></li>
-    <li>Price:{{ $comic->price }}</li>
-    <li>Series:{{ $comic->series }}</li>
-    <li>Release:{{ $comic->sale_date }}</li>
-    <li>Type:{{ $comic->type }}</li>
-    <li>Artist:{{ $comic->artists }}</li>
-    <li>Writers:{{ $comic->writers }}</li> 
-</ul>
+<div class="container">
+    <div class="w-50 mx-0">
+        <ul>
+            <li><strong>Title: </strong>{{ $comic->title }}</li>
+            <li><p><strong>Description: </strong>{{ $comic->description }}</p></li>
+            <li><img class="mb-3" src="{{$comic->thumb}}" alt="{{$comic->title}}" style="width: 12rem"></li>
+            <li><strong>Price: </strong>{{ $comic->price }}</li>
+            <li><strong>Series: </strong>{{ $comic->series }}</li>
+            <li><strong>Release: </strong>{{ $comic->sale_date }}</li>
+            <li><strong>Type: </strong>{{ $comic->type }}</li>
+            <li><strong>Artist: </strong>{{ $comic->artists }}</li>
+            <li><strong>Writers: </strong>{{ $comic->writers }}</li> 
+        </ul>
+
+        <a href="{{ route('comics.index') }}">
+            <div class="btn btn-secondary mb-3">Back</div>
+        </a>
+    </div>
+</div>
 @endsection
